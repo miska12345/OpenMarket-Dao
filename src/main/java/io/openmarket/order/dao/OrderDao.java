506c125d203c3f596581f02d7454cb9df970bc8c
2OP <po>
2020-09-24 21:44:15 -0700

    updated orderdao interface

--- src/main/java/io/openmarket/order/dao/OrderDao.java
+++ src/main/java/io/openmarket/order/dao/OrderDao.java
@@ -3,8 +3,10 @@ package io.openmarket.order.dao;
 import io.openmarket.dynamodb.dao.dynamodb.DynamoDBDao;
 import io.openmarket.order.model.Order;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface OrderDao extends DynamoDBDao<Order> {
     Optional<Order> getOrder(String orderId, String projection);
+    List<String> getOrderIdsByBuyer (String buyerId);
 }
