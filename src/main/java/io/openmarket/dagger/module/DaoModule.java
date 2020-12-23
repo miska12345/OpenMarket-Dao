79c6f5499cb68e8f0b47c7659cf060ee7c6e0d0b
ShangJin Li <shangli@ShangJins-MacBook-Pro.local>
2020-12-22 22:46:33 -0800

    Modify Dagger Module

--- src/main/java/io/openmarket/dagger/module/DaoModule.java
+++ src/main/java/io/openmarket/dagger/module/DaoModule.java
@@ -71,25 +71,6 @@ public class DaoModule {
 
     @Provides
     @Singleton
-    ComboPooledDataSource provideComboPooledDataSource() {
-        ComboPooledDataSource cpds = new ComboPooledDataSource();
-        cpds.setJdbcUrl(System.getenv("DB_URL"));
-        System.out.println(System.getenv("DB_URL"));
-        System.out.println(System.getenv("DB_USER"));
-        System.out.println(System.getenv("DB_PASS"));
-        cpds.setUser(System.getenv("DB_USER"));
-        cpds.setPassword(System.getenv("DB_PASS"));
-        cpds.setInitialPoolSize(5);
-        cpds.setMinPoolSize(5);
-        cpds.setAcquireIncrement(5);
-        cpds.setMaxPoolSize(20);
-        cpds.setMaxStatements(100);
-
-        return cpds;
-    }
-
-    @Provides
-    @Singleton
     StampEventDao provideEventDao(AmazonDynamoDB dbClient) {
         return new StampEventDaoImpl(dbClient, new DynamoDBMapper(dbClient));
     }
--- src/main/java/io/openmarket/dagger/module/MiscModule.java
+++ src/main/java/io/openmarket/dagger/module/MiscModule.java
@@ -1,6 +1,7 @@
 package io.openmarket.dagger.module;
 
 import com.google.gson.Gson;
+import com.mchange.v2.c3p0.ComboPooledDataSource;
 import dagger.Module;
 import dagger.Provides;
 
@@ -12,5 +13,23 @@ public class MiscModule {
     @Singleton
     Gson provideGson() {
         return new Gson();
+    }
+
+    @Provides
+    @Singleton
+    ComboPooledDataSource provideComboPooledDataSource() {
+        ComboPooledDataSource cpds = new ComboPooledDataSource();
+        cpds.setJdbcUrl(System.getenv("DB_URL"));
+        System.out.println(System.getenv("DB_URL"));
+        System.out.println(System.getenv("DB_USER"));
+        System.out.println(System.getenv("DB_PASS"));
+        cpds.setUser(System.getenv("DB_USER"));
+        cpds.setPassword(System.getenv("DB_PASS"));
+        cpds.setInitialPoolSize(5);
+        cpds.setMinPoolSize(5);
+        cpds.setAcquireIncrement(5);
+        cpds.setMaxPoolSize(20);
+        cpds.setMaxStatements(100);
+        return cpds;
     }
 }
