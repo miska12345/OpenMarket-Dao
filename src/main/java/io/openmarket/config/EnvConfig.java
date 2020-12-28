b196a3fe9a89f04574e76376dc58b3532ac12fc5
ShangJin Li <shangli@ShangJins-MacBook-Pro.local>
2020-12-28 13:39:39 -0800

    Use constant names for EV

---
+++ src/main/java/io/openmarket/config/EnvConfig.java
@@ -0,0 +1,18 @@
+package io.openmarket.config;
+
+public class EnvConfig {
+    /**
+     * The environment variable name for db url (ex. jdbc://...)
+     */
+    public static final String ENV_VAR_DB_URL = "DB_URL";
+
+    /**
+     * The environment variable name for db user.
+     */
+    public static final String ENV_VAR_DB_USER = "DB_USER";
+
+    /**
+     * The environment variable name for db password.
+     */
+    public static final String ENV_VAR_DB_PASSWORD = "DB_PASS";
+}
--- src/main/java/io/openmarket/dagger/module/MiscModule.java
+++ src/main/java/io/openmarket/dagger/module/MiscModule.java
@@ -1,13 +1,14 @@
 package io.openmarket.dagger.module;
 
 import com.google.gson.Gson;
-import com.mchange.v2.c3p0.ComboPooledDataSource;
 import dagger.Module;
 import dagger.Provides;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
 import javax.inject.Singleton;
+
+import static io.openmarket.config.EnvConfig.*;
 
 @Module
 public class MiscModule {
@@ -22,9 +23,9 @@ public class MiscModule {
     SessionFactory provideSessionFactory() {
         Configuration cfg = new Configuration();
         cfg.configure();
-        cfg.setProperty("hibernate.connection.url", System.getenv("DB_URL"));
-        cfg.setProperty("hibernate.connection.username", System.getenv("DB_USER"));
-        cfg.setProperty("hibernate.connection.password", System.getenv("DB_PASS"));
+        cfg.setProperty("hibernate.connection.url", System.getenv(ENV_VAR_DB_URL));
+        cfg.setProperty("hibernate.connection.username", System.getenv(ENV_VAR_DB_USER));
+        cfg.setProperty("hibernate.connection.password", System.getenv(ENV_VAR_DB_PASSWORD));
         cfg.setProperty("hibernate.dialect", "org.hibernate.dialect.MySQL5Dialect");
         return cfg.buildSessionFactory();
     }
